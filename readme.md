324485945808614af9ef12b165bf8fb466082c34
tiagoo <thurstaltamirano@escuelasproa.edu.ar>
2024-04-29 10:53:44 -0300

    anda

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Prueba Simon Dice
+
+Examen 29/04/2024
+
+## Instalación
+
+1. Clona el repo.
+2. Correlo con live server.
+3. Listo pa
+
+![Taiere](ruta/del/archivo.gif)
---
+++ script.js
@@ -0,0 +1,78 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const partes = document.querySelectorAll('.part');
+    const comenzarBtn = document.getElementById('comenzar');
+    const puntajeSpan = document.getElementById('puntaje');
+    let secuencia = [];
+    let puntaje = 0;
+    let jugando = false;
+    let intento = 0;
+
+    comenzarBtn.addEventListener('click', comenzarJuego);
+
+    partes.forEach(partes => {
+        partes.addEventListener('click', function() {
+            if (!jugando) return;
+
+            const idParte = partes.id;
+
+            if (idParte === secuencia[intento]) {
+                partes.classList.add('click');
+                setTimeout(() => partes.classList.remove('click'), 500);
+
+                intento++;
+                if (intento === secuencia.length) {
+                    puntaje++;
+                    puntajeSpan.textContent = puntaje;
+                    setTimeout(mostrarSecuencia, 1000);
+                    intento = 0;
+                }
+            } else {
+                alert('¡Has perdido! Puntaje: ' + puntaje);
+                reiniciarJuego();
+            }
+        });
+    });
+
+    function comenzarJuego() {
+        secuencia = [];
+        puntaje = 0;
+        puntajeSpan.textContent = puntaje;
+        jugando = true;
+        intento = 0;
+        mostrarSecuencia();
+    }
+
+    function reiniciarJuego() {
+        jugando = false;
+        comenzarBtn.disabled = false;
+    }
+
+    function mostrarSecuencia() {
+        comenzarBtn.disabled = true;
+        const colores = ['part1', 'part2', 'part3', 'part4'];
+        const nivelActual = secuencia.length + 1;
+        secuencia = [];
+        for (let i = 0; i < nivelActual; i++) {
+            const colorAleatorio = colores[Math.floor(Math.random() * 4)];
+            secuencia.push(colorAleatorio);
+        }
+        console.log('Secuencia:', secuencia);
+
+        let i = 0;
+        const intervalo = setInterval(() => {
+            const parte = document.getElementById(secuencia[i]);
+            if (parte) {
+                parte.classList.add('click');
+                setTimeout(() => {
+                    parte.classList.remove('click');
+                    i++;
+                    if (i >= secuencia.length) {
+                        clearInterval(intervalo);
+                        jugando = true;
+                        comenzarBtn.disabled = false;
+                    }
+                }, 500);
+            }
+        }, 1000);
+    }
+});
